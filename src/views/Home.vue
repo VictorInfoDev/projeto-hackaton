45a1606e27d7b49d8867999c236ea8dd4e45d7f0
Victor Hugo Casett Agostini <101749815+VictorInfoDev@users.noreply.github.com>
2022-06-11 10:46:38 -0300

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,10 +9,35 @@
             label="Procurar computador"
             single-line
             hide-details
-            color="success"
+            color="blue lighten-3"
           ></v-text-field>
         </v-card-title>
-        <v-card-text class=""><v-btn text outlined class="mr-2"><v-icon class="mr-1">mdi-cog</v-icon>Configurar computadores</v-btn><v-btn text outlined>Histórico de usos</v-btn></v-card-text>
+        <v-card-text class=""><v-btn text outlined class="mr-2" @click="dialogConfig = true"><v-icon class="mr-1">mdi-cog</v-icon>Configurar computadores</v-btn><v-btn text outlined>Histórico de usos</v-btn></v-card-text>
+        <v-card class="ma-4">
+        <v-simple-table dense>
+          <template v-slot:default>
+            <thead>
+              <tr>
+                <th class="text-left">
+                  Name
+                </th>
+                <th class="text-left">
+                  
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                v-for="item in desserts"
+                :key="item.name"
+              >
+                <td>{{ item.name }}</td>
+                <td>{{ item.calories }}</td>
+              </tr>
+            </tbody>
+          </template>
+        </v-simple-table>
+      </v-card>
   </v-card>
   <div class="ml-10"><h1>Status</h1></div>
   <v-row>
@@ -44,6 +69,62 @@
     </v-hover>
     </v-col>
   </v-row>
+  <v-dialog v-model="dialogConfig" max-width="700px">
+    <v-card>
+      <v-card-title class="blue lighten-3 pa-5">
+        <v-icon class="mr-2" color="black">mdi-cog</v-icon>Configurar computadores
+        <v-spacer></v-spacer>
+        <v-icon @click="dialogConfig = false" color="black" >mdi-close</v-icon>
+      </v-card-title>
+      <v-card-text>
+        <v-simple-table
+          fixed-header
+          height="300px"
+        >
+          <template v-slot:default>
+            <thead>
+              <tr>
+                <th class="text-left ">
+                  <h2>Nome do computador</h2> 
+                </th>
+                <th class="text-right">
+                  
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>PC 04</td>
+                <td class="text-right"><v-icon color="warning">mdi-alert</v-icon><v-icon color="success" class="ml-2">mdi-check</v-icon><v-icon color="error" class="ml-2" @click="test()">mdi-trash-can-outline</v-icon></td>
+              </tr>
+              <tr
+                v-for="item in desserts"
+                :key="item.name"
+              >
+                <td>{{ item.name }}</td>
+                <td class="text-right"><v-icon color="gray" class="ml-2" @click="test()">mdi-wrench-outline</v-icon><v-icon color="error" class="ml-2" @click="test()">mdi-trash-can-outline</v-icon></td>
+              </tr>
+            </tbody>
+          </template>
+        </v-simple-table>
+      </v-card-text>
+      <v-card-text class="grey darken-4 text-center pt-4" dark><v-btn @click="dialogCadastroPC = true, dialogConfig = false" class="grey darken-4 white--text" elevation="0"><h2><v-icon large color="white">mdi-plus</v-icon>Cadastrar computador</h2></v-btn></v-card-text>
+    </v-card>
+  </v-dialog>
+  <v-dialog max-width="700px" v-model="dialogCadastroPC">
+    <v-card>
+      <v-card-title class="white--text grey darken-4">
+        <v-icon large color="white" class="mr-2">mdi-plus</v-icon>Cadastrar computadores
+      </v-card-title>
+      <v-card-text>
+        <v-row class="mt-5">
+          <v-col cols="12" sm="6"><v-text-field label="Nome do computador" outlined></v-text-field></v-col>
+          <v-col cols="12" sm="6"><v-text-field label="N° de ativo" outlined></v-text-field></v-col>
+        </v-row>
+      </v-card-text>
+      <v-card-actions><v-spacer></v-spacer><v-btn @click="dialogConfig = true, dialogCadastroPC = false">Cancelar</v-btn><v-btn class="ml-2" dark>Cadastrar</v-btn></v-card-actions>
+    </v-card>
+  </v-dialog>
   </v-app>
 </template>
 
@@ -51,16 +132,55 @@
 export default {
   data(){
     return{
+      dialogConfig: false,
+      dialogCadastroPC: false,
       pc:[
         {nome: "PC 01", nomeuser:"Felipe Mendes", hre:"18:30", hrs:"19:30", status:"Indisponível", color:'error', projeto:"Hackaton"},
         {nome: "PC 02", nomeuser:"Nenhum", hre:"Livre", hrs:"Livre", status:"Disponível", color:'success', projeto:"Nenhum"},
         {nome: "PC 03", nomeuser:"*", hre:"*", hrs:"*", status:"Manutenção", color:'warning', projeto:"*"}
-      ]
+      ],
+desserts: [
+          {
+            name: 'PC 01',
+            
+          },
+          {
+            name: 'PC 02',
+ 
+          },
+          {
+            name: 'PC 03',
+            
+          },
+          {
+            name: 'PC 05',
+          
+          },
+          {
+            name: 'PC 06',
+            
+          },
+          {
+            name: 'PC 07',
+            
+          },
+          {
+            name: 'PC 08',
+            
+          },
+          {
+            name: 'PC 09',
+            
+          },
+          {
+            name: 'PC 10',
+            
+          },
+        ],
     }
   }
 }
 </script>
 
 <style>
-
 </style>
